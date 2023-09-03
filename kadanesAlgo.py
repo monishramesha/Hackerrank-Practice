8790b892d3213564c28e40663c63eecdc6ad20ab
Monish Ramesha <70026302+monishramesha@users.noreply.github.com>
2023-09-03 18:42:26 +0530

    Create kadanesAlgo.py
    Given an integer array nums, find the 
    subarray
     with the largest sum, and return its sum.

---
+++ kadanesAlgo.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxi = -sys.maxsize-1
+        sum = 0
+        n = len(nums)
+        for i in range(n):
+            sum += nums[i]
+
+            if sum > maxi:
+                maxi = sum
+
+            if sum < 0:
+                sum = 0
+        if maxi < 0: maxi = 0
+
+        return maxi
